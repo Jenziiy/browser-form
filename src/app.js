775e894d7821fc738970399746939dfc350cbdcd
user.email <developerjenzii@gmail.com>
2023-10-19 18:44:49 +0200

    add zipcode checker

--- src/app.js
+++ src/app.js
@@ -33,3 +33,40 @@ password.addEventListener("input", () => password.setCustomValidity('') );
 confirmPassword.addEventListener("blur", confirmThePassword.bind( this, password,confirmPassword ) );
 confirmPassword.addEventListener("input", () => confirmPassword.setCustomValidity('') );
 
+function checkZIP(){
+  // For each supported country, define the pattern the ZIP code has to match.
+
+  const constraints = {
+    nl: ["^(NL-)?\\d{4}\\s*([A-RT-Z][A-Z]|S[BCE-RT-Z])$",
+        "Nederlandse postcodes bestaan uit 4 nummers, gevolgd door 2 letters. Geen SA, SD, SS",
+      ],
+  }
+
+
+  // read country id value
+  const country = document.getElementById('country').value;
+
+  // get field
+  const ZIPField = document.getElementById("zipcode");
+
+  // constraint checker
+  const constraint = new RegExp(constraints[country][0], "");
+  console.log(constraint);
+
+  //check
+
+  if (constraint.test(ZIPField.value)) {
+    //zip matches patterns
+    ZIPField.setCustomValidity("");
+    ZIPField.reportValidity();
+  } else {
+    // zip is incorrect
+  ZIPField.setCustomValidity(constraints[country][1])
+  ZIPField.reportValidity();
+  }
+}
+
+window.onload = () => {
+  document.getElementById("country").onchange = checkZIP;
+  document.getElementById("zipcode").oninput = checkZIP;
+}
